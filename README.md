86804e0ab8e56551c10e93f09dd6938956f0ad8c
gosplit <gosplit@yahoo.com.tw>
2022-10-01 03:06:05 +0800

    update with singapore endpoint

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# taiwan-holidays
-台灣節日的MongoDB和 Restful Data API，可以使用mongodb進行查詢/篩選
+# taiwan-holidays-mongodb
+台灣節日的MongoDB和 Restful Data API，可以使用mongodb或 Restful API 進行查詢/篩選
+
+由於現有的 Public Opendata API (台北市/新北市)都是分頁格式，必須把所有資料都拉下來才能處理。 如果只是想要查詢特定的節假日，或是想要找「連續假日」都很麻煩。所以把資料整理了一下放到MongoDB方便查詢。 目前從2013到2023的資料都已經更新了，預計每季會跑一次data update。
+
+## 連線資訊
+  - **cluster url** `public.vovmgbr.mongodb.net`
+  - **db** `public`
+  - **collection**: `taiwan-holidays`
+  - **username:** `taiwan`
+  - **password:** `public`
+
+  - **DataAPI endpoint:** `https://southeastasia.azure.data.mongodb-api.com/app/data-pjagx/endpoint/data/v1`
+  - **dataSource** `public`
+  - **api-key:** `YsM3npR5ipSE052zu9h1epaa4Vo3OB0AFvlPL7mTH0Gw8yzU4hI02p2xsZa6zMqB`
+  
+  
+##  資料說明範例
+  
+```json
+{
+  "_id": "2023/1/1", 
+  "date": "2023-01-01T00:00:00.000+00:00",
+  "description": "全國各機關學校放假一日。",
+  "year": 2023,
+  "month": 1,
+  "category": "放假之紀念日及節日",
+  "consecutive": true //是否為連續假日(三天以上)
+}
+```
+
+## 查詢範例
+1. 使用 python MongoDB driver: 查詢今年和明年的所有假日
+```python
+from pymongo import MongoClient
+import datetime
+client = MongoClient("mongodb+srv://taiwan:public@public.vovmgbr.mongodb.net")
+collection = client["public"]["taiwan-holidays"]
+cursor = collection.find({ "year": {"$gte": datetime.datetime.now().year}})
+for document in cursor:
+     print(document)
+```
+
+2. 使用curl 查詢2022年的所有「連續假日」
+```shell
+curl --location --request POST 'https://southeastasia.azure.data.mongodb-api.com/app/data-pjagx/endpoint/data/v1/action/find' \
+--header 'Content-Type: application/json' \
+--header 'Access-Control-Request-Headers: *' \
+--header 'api-key: YsM3npR5ipSE052zu9h1epaa4Vo3OB0AFvlPL7mTH0Gw8yzU4hI02p2xsZa6zMqB' \
+--data-raw '{
+    "collection":"taiwan-holidays",
+    "database":"public",
+    "dataSource":"public",
+    "filter": { "year": 2022, "consecutive": true}
+}'
+```
+
+### 其他MongoDB查詢範例
+檢查該日(yyyy/m/d)是否為已知假日
+```json
+{ "_id": "2017/1/1" }
+```
+
+查詢中秋節
+```json
+{ "name":"中秋節" }
+```
+
+查詢 2022年，所有假日排除週末
+```json
+{ "year": 2022, "category": { "$ne": "週末"} }
+```
+
+## 免責聲明
+Cluster是使用MongoDB Altas的免費空間@GCP Taiwan datacenter，所以用MongoDB driver 連線速度比較快。而DataAPI在新加坡，所以用RESTful API會比較慢。
+但這都是用free shared resource，所以不保證多穩定就是了...
+
+
+
---
+++ src/taiwan-holiday-collector.py
@@ -0,0 +1,77 @@
+from pymongo import MongoClient
+from sqlite3 import Date
+from urllib.request import urlopen
+import datetime
+import json
+url = "https://data.ntpc.gov.tw/api/datasets/308DCD75-6434-45BC-A95F-584DA4FED251/json?page={}&size=300"
+
+stillHasContent = True
+pageIndex = 0
+holidays = []
+
+check_duplicate = {}
+
+
+def transform(opendataItem):
+    if (opendataItem["date"] in check_duplicate):
+        return None
+    else:
+        check_duplicate[opendataItem["date"]] = opendataItem
+    if ("date" in opendataItem):
+        opendataItem["_id"] = opendataItem["date"]
+        opendataItem["date"] = datetime.datetime.strptime(
+            opendataItem["date"], "%Y/%m/%d")
+        opendataItem["year"] = opendataItem["date"].year
+        opendataItem["month"] = opendataItem["date"].month
+    opendataItem.pop('isHoliday', None)
+    if ("name" in opendataItem and opendataItem["name"] == ""):
+        opendataItem.pop('name', None)
+    if ("description" in opendataItem and opendataItem["description"] == ""):
+        opendataItem.pop('description', None)
+    if ("holidayCategory" in opendataItem):
+        opendataItem["category"] = (
+            "週末" if opendataItem["holidayCategory"] == "星期六、星期日" else opendataItem["holidayCategory"])
+        opendataItem.pop('holidayCategory', None)
+    return opendataItem
+
+
+def isConsecutive(opendataItem):
+    if ("consecutive" in opendataItem):
+        return opendataItem["consecutive"]
+    return False
+
+
+# store the response of URL
+while stillHasContent:
+    response = urlopen(url.format(pageIndex))
+    pageIndex += 1
+    data_json: list = json.loads(response.read())
+    holidays.extend([x for x in map(transform, data_json) if x is not None])
+    stillHasContent = (len(data_json) == 300)
+
+previous_holiday = holidays[0]["date"]
+holiday_count = 1
+for i in range(1, len(holidays)):
+    delta = holidays[i]["date"] - previous_holiday
+    if (delta.days == 1):
+        holiday_count += 1
+        previous_holiday = holidays[i]["date"]
+    elif (holiday_count >= 3):
+        for j in range(0, holiday_count):
+            holidays[i-j-1]["consecutive"] = True
+        holiday_count = 1
+        previous_holiday = holidays[i]["date"]
+    else:
+        holiday_count = 1
+        previous_holiday = holidays[i]["date"]
+
+if (holiday_count >= 3):
+    for j in range(0, holiday_count):
+        holidays[-j-1]["consecutive"] = True
+
+print(holidays)
+
+client = MongoClient("mongodb+srv://admin:<adminpassword>@public.vovmgbr.mongodb.net/?retryWrites=true&w=majority")
+collection = client["public"]["taiwan-holidays"]
+collection.delete_many({})
+collection.insert_many(holidays, ordered=True, bypass_document_validation=False, session=None)
